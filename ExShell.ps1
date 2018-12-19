91c0a916fc1d62cd417d4c42ff511fff8d455b99
RamanGoswami <raman.goswami@experteq.com>
2018-12-19 12:27:26 +1100

    Create ExShell.ps1

---
+++ ExShell.ps1
@@ -0,0 +1,12 @@
+﻿#$UserCredential = Get-Credential
+
+$user = "raman.goswam@axicorp.com"
+$password = "AF12passrg"
+$secureStringPwd = $password | ConvertTo-SecureString -AsPlainText -Force 
+$UserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $user, $secureStringPwd
+
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -AllowClobber
+import-module AzureRM
+Connect-MsolService -Credential $UserCredential
